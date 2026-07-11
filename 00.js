b2f2886fae6e42dbe57fff6f6fd9f98099f9fa00
Mauricio Alvarez <297007275+MarAlvGEN@users.noreply.github.com>
2026-07-11 17:21:10 -0500

    FINISH: 1
    Hago una logica en base a funciones.
    
    TODO:
    - Debo buscar una forma de no repetir tantas veces el if en vocales.
    - Debo hacer todo en una funcion llamda "analizarCadena"

--- 00.js
+++ 00.js
@@ -1,20 +1,39 @@
-let texto = "Holaaaaaaaa estooo pruebaaaa";
-let totalCarac = 0;
+let texto = "a bb cc ddd eee ffff";
 
-// let textoTotal = texto.length();
+let totalCarac = contarCaracteres(texto);
 
 var vocales = [];
 vocalesTolal = vocalesTolal(texto);
 
-let caracSinEspacios = texto.replaceAll(" ", "");
+let caracRepetidos = conCaracteresRepetidos(texto);
 
-console.log(caracSinEspacios);
+// MAIN
 
-console.log(vocales);
+console.log(totalCarac);
 console.log(vocalesTolal);
 
-console.log(texto.length);
+console.log(`--Cantidad de Caracteres en "${texto}"`);
+for (const key in caracRepetidos) {
+  console.log(`${key}: ${caracRepetidos[key]} `);
+}
 
+// Funciones
+
+// Parte I: Esta funcion debe contar la cantidad de caracteres
+// En esta ocasion es recibe un texto, se pasa a toLowerCase y elimina todos los textos en blanco
+// retorna la total contado
+
+function contarCaracteres(texto) {
+  let textoCont = 0;
+  textoAux = texto.replaceAll(" ", "");
+
+  for (let i = 0; i < textoAux.length; i++) {
+    textoCont++;
+  }
+  return `--CANTIDAD CARACTERES EN: "${texto}"--\nCon espacios: "${texto.length}"\nSin espacios: "${textoCont}"`;
+}
+
+// Parte II: Esta funcion debe contar la cantidad de vocales que tiene el texto ademas quiero que muestre el proceso con un console.log()
 function vocalesTolal(texto) {
   textoAux = texto.toLowerCase();
 
@@ -31,38 +50,17 @@ function vocalesTolal(texto) {
       vocales.push(textoAux[i]);
     }
   }
-  return vocales.length;
+  return `--CANTIDAD VOCALES-- en "${texto}"\nTotal: "${vocales.length}"`;
 }
 
-let contCaract = {};
-contarCaracteres(texto);
+// Parte III: Esta funcion debe contar la cantidad de veces que los caracteres se repiten
 
-for (const key in contCaract) {
-  console.log(key, contCaract[key]);
-}
+function conCaracteresRepetidos(texto) {
+  let caracRepetidos = {};
+  let textoAux = texto.replaceAll(" ", "");
 
-function contarCaracteres(texto) {
-  texto = texto.trim().toLowerCase().replaceAll(" ", "");
-
-  for (const key of texto.trim().replaceAll(" ", "").toLowerCase()) {
-    contCaract[key] = (contCaract[key] || 0) + 1;
+  for (const key of textoAux) {
+    caracRepetidos[key] = (caracRepetidos[key] || 0) + 1;
   }
+  return caracRepetidos;
 }
-
-console.log(contCaract);
-
-// for (const key in texto.trim()) {
-//   if (object.hasOwnProperty(key)) {
-//     const element = object[key];
-//   }
-// }
-
-// function contarCaracteres(texto) {
-//   texto = texto.trim();
-//   contChar = 0;
-//
-//   for (let i = 0; i < texto.length; i++) {
-//     contChar++;
-//   }
-//   return contChar;
-// }
